e088cafb32e9da23bba47ac0b6e940eb64924329
Jonathan Witmore <heratiki@gmail.com>
2024-05-06 00:48:29 -0400

    refactor: Update GUI layout to include 3D plot of STL file

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import PySimpleGUI as sg
 import trimesh
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # Define the layout of the GUI
 gui_layout = [
     [sg.Text("Choose an STL file:")],
     [sg.Input(key="STL_FILE_PATH"), sg.FileBrowse()],
-    [sg.Button("Process STL File")],
-    [sg.Graph((800, 600), (0, 0), (800, 600), key="GRAPH_DISPLAY")]
+    [sg.Button("Process STL File")]
 ]
 
 # Create the window
@@ -22,7 +23,16 @@ while True:
             stl_mesh = trimesh.load(stl_file_path)
             if isinstance(stl_mesh, list):
                 stl_mesh = stl_mesh[0]  # Get the first mesh from the list
-            stl_mesh.show()  # Show the 3D viewer
+
+            # Create a new figure for the 3D plot
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+
+            # Plot the vertices of the mesh
+            ax.scatter(stl_mesh.vertices[:,0], stl_mesh.vertices[:,1], stl_mesh.vertices[:,2])
+
+            # Show the 3D plot
+            plt.show()
 
 # Close the window
 gui_window.close()

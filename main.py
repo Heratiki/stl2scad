64fa2dea3389840a7b5a7bdb6e1dfe66789247c5
Jonathan Witmore <heratiki@gmail.com>
2024-05-08 01:05:51 -0400

    refactor: Update main.py to use PyQt5 and pyqtgraph for displaying STL files

--- main.py
+++ main.py
@@ -1,44 +1,35 @@
-import PySimpleGUI as sg
 import numpy as np
 from stl import mesh
-from mpl_toolkits import mplot3d
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from PyQt5 import QtWidgets
+import pyqtgraph.opengl as gl
+import sys
 
-# Define the layout of the GUI
-gui_layout = [
-    [sg.Text("Choose an STL file:")],
-    [sg.Input(key="STL_FILE_PATH"), sg.FileBrowse()],
-    [sg.Button("Process STL File")],
-    [sg.Canvas(key='CANVAS', size=(500, 500))],  # Specify a size for the canvas
-]
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
 
-# Create the window
-gui_window = sg.Window('STL File Processor', gui_layout)
+        self.gl_view = gl.GLViewWidget()
+        self.setCentralWidget(self.gl_view)
 
-while True:
-    event, values = gui_window.read()
-    if event == sg.WINDOW_CLOSED:
-        break
-    elif event == "Process STL File":
-        stl_file_path = values["STL_FILE_PATH"]
-        if stl_file_path:
-            # Load the STL files and add the vectors to the plot
-            your_mesh = mesh.Mesh.from_file(stl_file_path)
-            figure = plt.figure()
-            axes = figure.add_subplot(projection='3d')
-            axes.set_box_aspect([np.ptp(a) for a in (your_mesh.x.flatten(), your_mesh.y.flatten(), your_mesh.z.flatten())])
-            poly_collection = mplot3d.art3d.Poly3DCollection(your_mesh.vectors)
-            poly_collection.set_color((0.7,0.7,0.7))  # play with color
-            axes.add_collection3d(poly_collection)
+        self.open_file_action = QtWidgets.QAction("Process STL File", self)
+        self.open_file_action.triggered.connect(self.load_stl_file)
 
-            # Auto scale the axes to fit the data
-            axes.auto_scale_xyz(your_mesh.x.flatten(), your_mesh.y.flatten(), your_mesh.z.flatten())
+        self.file_menu = self.menuBar().addMenu("&File")
+        self.file_menu.addAction(self.open_file_action)
 
-            # Add the plot to the PySimpleGUI window
-            canvas = FigureCanvasTkAgg(figure, master=gui_window['CANVAS'].TKCanvas)
-            canvas.draw()
-            canvas.get_tk_widget().pack(side='top', fill='both', expand=True)  # Pack the canvas into the GUI
+    def load_stl_file(self):
+        file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open STL File", "", "STL Files (*.stl)")
+        if file_path:
+            your_mesh = mesh.Mesh.from_file(file_path)
+            vertices = np.concatenate(your_mesh.vectors)
+            faces = np.array([(i, i+1, i+2) for i in range(0, len(vertices), 3)])
+            mesh_data = gl.MeshData(vertexes=vertices, faces=faces)
+            mesh_item = gl.GLMeshItem(meshdata=mesh_data, color=(0.7, 0.7, 0.7, 1.0), smooth=False)
+            self.gl_view.addItem(mesh_item)
 
-# Close the window
-gui_window.close()
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+sys.exit(app.exec_())

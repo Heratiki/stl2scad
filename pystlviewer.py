04f5f8f716552756e35b74587b6e4dc015a0af25
Jonathan Witmore <heratiki@gmail.com>
2024-05-07 23:04:59 -0400

    refactor: Update pystlviewer.py to import mpl_toolkits correctly for 3D plot support

--- pystlviewer.py
+++ pystlviewer.py
@@ -1,8 +1,8 @@
 # import numpy as np
 from stl import mesh
 import matplotlib.pyplot as plt
-import mpl_toolkits
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits import mplot3d    # This is required before you can load Axes3D  unless you load it directly with from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d import Axes3D
 
 def display_stl(filename):
     # Load the STL files and add the vectors to the plot
@@ -11,7 +11,7 @@ def display_stl(filename):
     axes = mplot3d.Axes3D(figure)
 
     # Add the vectors to the plot
-    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors, edgecolor='k'))
 
     # Auto scale to the mesh size
     scale = your_mesh.points.flatten()
